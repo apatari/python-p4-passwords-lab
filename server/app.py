ecce8580c559651b10e7ddf56bbfb7407b0093c6
Andy Patari <andy.patari@gmail.com>
2023-11-07 13:46:14 -0500

    passing tests

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import request, session
+from flask import request, session, make_response
 from flask_restful import Resource
 
 from config import app, db, api
@@ -20,24 +20,51 @@ class Signup(Resource):
     def post(self):
         json = request.get_json()
         user = User(
-            username=json['username'],
-            password_hash=json['password']
+            username=json.get("username"),
+            password_hash=json.get("password")
         )
         db.session.add(user)
         db.session.commit()
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    
+    def get(self):
+
+        user = User.query.filter_by(id=session.get('user_id')).first()
+
+        if user:
+            return user.to_dict(), 200
+        else:
+            return {}, 204
+
 
 class Login(Resource):
-    pass
+    
+    def  post(self):
+        username = request.get_json().get('username')
+        user = User.query.filter_by(username=username).first()
+        password = request.get_json()['password']
+
+        if user.authenticate(password):
+            session['user_id'] = user.id
+            return make_response(user.to_dict(), 200)
+
+        return {'error': 'Invalid username or password'}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session['user_id'] = None
+
+        response = make_response({"message": "No content"}, 204)
+
+        return response
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
